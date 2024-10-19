c6ea2fd2500a1e0431a213da82ed1736dae7de31
Peter Shaba <petershababladez@gmail.com>
2024-10-18 19:30:00 -0500

    test

--- server/controllers/VolunteerMatchingController.js
+++ server/controllers/VolunteerMatchingController.js
@@ -1,11 +1,11 @@
 // controllers/VolunteerMatchingController.js
 
-const VolunteerMatcher = require('../services/volunteerMatcher');
+//const VolunteerMatcher = require('../services/volunteerMatcher');
 
 // Match volunteers for an event
 exports.matchVolunteers = (req, res) => {
     const { volunteers, events } = req.body;
-    const matcher = new VolunteerMatcher(volunteers, events);
+   // const matcher = new VolunteerMatcher(volunteers, events);
     const matches = matcher.matchVolunteers();
     res.status(200).json({ matches });
 };
