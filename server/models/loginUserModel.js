e85741c4deb0eaf9acf5a64a24470489ca1abf5a
Peter Shaba <60669926+pshaba@users.noreply.github.com>
2024-11-14 21:59:20 -0600

    Update loginUserModel.js

--- server/models/loginUserModel.js
+++ server/models/loginUserModel.js
@@ -2,30 +2,63 @@
 
 //In-memory storage for demo
 //will adjust once database is set up
-const users = []; 
+//const users = []; //removed now that database has been implementeed//Database Implemented 
+
+require('dotenv').config(); //load environment variables
+const db = require("../database"); // Use require instead of import -- database.js
 
 const User = {
-    findUser: (email) => users.find(user => user.email === email), 
+    findUser: async (email) => { //made async when database implemented
+        //users.find(user => user.email === email) // demo removed now that database has been implemented
+
+        try {
+            const [rows] = await db.query('SELECT * FROM userCredentials WHERE email = ?', [email]);
+            return rows[0];
+        } catch (error) {
+            console.error('Database query error:', error);
+            throw error; 
+        }
+        
+    }, 
     
-    addUser: (user) => {
-        user.profileComplete = false; //set default value of profile complete to false
-        users.push(user); 
-    }, 
+    addUser: async (user) => { //made async when database implemented
+        //user.profileComplete = false; //set default value of profile complete to false
+        //users.push(user); 
 
-    isProfileComplete: (email) => {
-        const user = User.findUser(email); 
-        return user ? user.profileComplete : false; 
-    }, 
-
-    setProfileComplete: (email) => {
-        const user = User.findUser(email); 
-        if (user) {
-            user.profileComplete = true; 
+        //database implemntation
+        try {
+            const {email, password} = user; 
+            await db.query('INSERT INTO userCredentials (email, password) VALUES (?, ?)', [email, password]); 
+        } catch (error) {
+            console.error('Database query error:', error);
+            throw error;
         }
     }, 
 
-    __resetUsers: () => {
-        users.length = 0; // Clear users for testing
+    isProfileComplete: async (email) => { //made async when database implemented
+        const user = await User.findUser(email); //added await when database implemnted, everything else stayed same
+        if (!user) {
+            return false; 
+        }
+
+        console.log("Is profile complete? ", user.profileComplete); // 0 for not; 1 for true
+        return user.profileComplete === 1; 
+        //return user.profileComplete ? user : 0;
+    }, 
+
+    setProfileComplete: async (email) => { //made async when database implemented
+        /*const user = User.findUser(email); 
+        if (user) {
+            user.profileComplete = true; 
+        }*/
+
+        await db.query('UPDATE usersCredentials SET profileComplete = ? WHERE email = ?', [1, email]); 
+    }, 
+
+    __resetUsers: async () => { //made async when database implemented
+        //users.length = 0; // Clear users for testing -- used during demo replaced once database implemented
+
+        await db.query('DELETE FROM usersCredentials'); 
     }
 }; 
 
